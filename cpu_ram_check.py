103c063813b13450eff2d23adb7a440130122acf
Namianse <nathanserra29@gmail.com>
2026-03-06 14:49:00 +0100

    Update cpu_ram_check.py

--- cpu_ram_check.py
+++ cpu_ram_check.py
@@ -1,12 +1,9 @@
 import psutil
 import time
 
-while(True){
+while True:
     cpu = psutil.cpu_percent(interval=1)
     ram = psutil.virtual_memory()
-
     print(f"CPU : {cpu}%")
     print(f"RAM : {ram.percent}% utilisée ({round(ram.used / 1024**3, 1)} Go / {round(ram.total / 1024**3, 1)} Go)")
-
     time.sleep(3)
-}
