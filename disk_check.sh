6d5fc5a7d510315db6b90b0dba20f2ef9fad7b35
Namianse <nathanserra29@gmail.com>
2026-03-06 14:57:35 +0100

    Update disk_check.sh

--- disk_check.sh
+++ disk_check.sh
@@ -1,4 +1,1 @@
-while true; do
-    df --total -h | awk '/^total/ {print "Espace disque disponible : " $4 "\nEspace disque utilisé : " $3 ", " $5}'
-    sleep 30
-done
+df --total -h | awk '/^total/ {print "Espace disque disponible : " $4 "\nEspace disque utilisé : " $3 ", " $5}'
