79b9d1a0cd2263b415538c27a82b135d3d9789de
Namianse <nathanserra29@gmail.com>
2026-03-13 16:56:28 +0100

    Create crise.py

---
+++ crise.py
@@ -0,0 +1,35 @@
+import sqlite3
+import json
+
+with open('/home/dieu/AMS-outil/config.json', 'r') as f:
+    config = json.load(f)
+
+conn = sqlite3.connect('/home/dieu/AMS-outil/monitoring.db')
+cursor = conn.cursor()
+
+cursor.execute('SELECT * FROM metrics ORDER BY rowid DESC LIMIT 1')
+result = cursor.fetchone()
+
+if result is None:
+    print("Aucune donnée")
+else:
+    timestamp, cpu, ram, disque_pct = result
+
+    cpu_str    = f"CPU : {cpu}%    {'/!\' if cpu > config['seuil_cpu'] else ''}"
+    ram_str    = f"RAM : {ram}%    {'/!\' if ram > config['seuil_ram'] else ''}"
+    disque_str = f"Disque : {disque_pct}%    {'/!\' if disque_pct > config['seuil_disque'] else ''}"
+
+    crise = any([
+        cpu > config['seuil_cpu'],
+        ram > config['seuil_ram'],
+        disque_pct > config['seuil_disque']
+    ])
+
+    if crise:
+        print("/!\ Crise détectée")
+    else:
+        print("Aucun problème détectée.")
+
+    print(cpu_str)
+    print(ram_str)
+    print(disque_str)
