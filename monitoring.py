7a505498e56f5deb33653a87613272c0edbf8fb3
Namianse <nathanserra29@gmail.com>
2026-03-13 14:59:31 +0100

    Update monitoring.py

--- monitoring.py
+++ monitoring.py
@@ -29,12 +29,5 @@ def insert():
     ))
     conn.commit()
 
-def display():
-    cursor.execute('SELECT * FROM metrics')
-    results = cursor.fetchall()
-    for row in results:
-        print(f"{row[0]} | CPU: {row[1]}% | RAM: {row[2]}% | Disque: {row[3]}%")
-
 clean()
 insert()
-display()
