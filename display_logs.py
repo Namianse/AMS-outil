0cb2e467a1f4ab842d6dab4370505fe072a3ede4
Namianse <nathanserra29@gmail.com>
2026-03-13 16:26:10 +0100

    Update display_logs.py

--- display_logs.py
+++ display_logs.py
@@ -8,5 +8,8 @@ def display():
     results = cursor.fetchall()
     for row in results:
         print(f"{row[0]} | CPU: {row[1]}% | RAM: {row[2]}% | Disque: {row[3]}%")
+    cursor.execute('SELECT * FROM alertes')
+    result = cursor.fetchone()
+    print(f"Dernière alerte : {result[0]} | Lien : {result[1]}")
 
 display()
