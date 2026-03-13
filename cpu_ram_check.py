6dde685d8a386deddbbc469600242901bd975f2e
Namianse <nathanserra29@gmail.com>
2026-03-13 14:17:50 +0100

    Update cpu_ram_check.py

--- cpu_ram_check.py
+++ cpu_ram_check.py
@@ -1,9 +1,15 @@
 import psutil
-import time
 import datetime
+import json
 
 cpu = psutil.cpu_percent(interval=1)
 ram = psutil.virtual_memory()
-print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
-print(f"CPU : {cpu}%")
-print(f"RAM : {ram.percent}% utilisée ({round(ram.used / 1024**3, 1)} Go / {round(ram.total / 1024**3, 1)} Go)")
+
+data = {
+    "timestamp": datetime.datetime.now().isoformat(timespec='seconds'),
+    "cpu": cpu,
+    "ram": ram.percent
+}
+
+with open('cpu_ram.json', 'w') as f:
+    json.dump(data, f)
