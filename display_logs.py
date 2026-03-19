6c3cc333774bdb2ccdac5f0beb329c1a78f953c2
Namianse <nathanserra29@gmail.com>
2026-03-19 18:22:18 +0100

    Update display_logs.py

--- display_logs.py
+++ display_logs.py
@@ -7,7 +7,7 @@ def display():
     cursor.execute('SELECT * FROM metrics')
     results = cursor.fetchall()
     for row in results:
-        print(f"{row[0]} | CPU: {row[1]}% | RAM: {row[2]}% | Disque: {row[3]}%")
+        print(f"MACHINE: {row[4]} | {row[0]} | CPU: {row[1]}% | RAM: {row[2]}% | Disque: {row[3]}%")
     cursor.execute('SELECT * FROM alertes ORDER BY rowid DESC LIMIT 1')
     result = cursor.fetchone()
     print(f"Dernière alerte : {result[0]} | Lien : {result[1]}")
